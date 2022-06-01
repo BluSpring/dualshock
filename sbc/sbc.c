5c1596cc69a6931ccaefa5a35c64f69721caf145
Naz <bluspringindohq@gmail.com>
2022-06-01 21:07:09 +0800

    Actually wait this is reverting some stuff

--- sbc/sbc.c
+++ sbc/sbc.c
@@ -92,7 +92,7 @@ struct a2dp_sbc {
 	uint8_t allocation_method:2;
 	uint8_t min_bitpool;
 	uint8_t max_bitpool;
-} __attribute__ ((packed));
+} __attribute__ (packed);
 
 #else
 #error "Unknown byte order"
